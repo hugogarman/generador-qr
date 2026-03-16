de7c2b5d678f38c2d0b8f0cd43857164bb7e1cb9
Hugo García <hugo.garman.2@educa.jcyl.es>
2026-03-16 18:08:46 +0100

    Update mobile-menu.js

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -2,10 +2,46 @@ const toggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 const close = document.querySelector(".close-menu");
 
-toggle.addEventListener("click", () => {
-  menu.classList.add("open");
+toggle.onclick = () => {
+menu.classList.add("open");
+};
+
+close.onclick = () => {
+menu.classList.remove("open");
+};
+
+/* abrir panel */
+
+document.querySelectorAll(".has-next").forEach(link => {
+
+link.onclick = () => {
+
+const target = link.dataset.target;
+
+document.querySelectorAll(".mobile-panel").forEach(panel=>{
+panel.classList.remove("active");
 });
 
-close.addEventListener("click", () => {
-  menu.classList.remove("open");
+document.querySelector("."+target).classList.add("active");
+
+};
+
+});
+
+/* volver atrás */
+
+document.querySelectorAll(".back").forEach(btn=>{
+
+btn.onclick = () => {
+
+const back = btn.dataset.back;
+
+document.querySelectorAll(".mobile-panel").forEach(panel=>{
+panel.classList.remove("active");
+});
+
+document.querySelector("."+back).classList.add("active");
+
+};
+
 });
