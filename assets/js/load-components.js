2512686543ef0266c636e6f91607d554155ff25c
Hugo García <hugo.garman.2@educa.jcyl.es>
2026-03-17 12:49:32 +0100

    Update load-components.js

--- assets/js/load-components.js
+++ assets/js/load-components.js
@@ -1,13 +1,16 @@
 // HEADER
 fetch("/components/header.html")
-  .then(res => res.text())
-  .then(data => {
+  .then((res) => res.text())
+  .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
 
 // MOBILE MENU
 fetch("/components/mobile-menu.html")
-  .then(res => res.text())
-  .then(data => {
+  .then((res) => res.text())
+  .then((data) => {
     document.getElementById("mobile-menu-container").innerHTML = data;
+
+    // 🔥 IMPORTANTE: inicializar menú después de cargarlo
+    initMobileMenu();
   });
